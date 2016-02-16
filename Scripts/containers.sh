6f33f264eb67bc4bb0e68ad0b37cd03bfaf64169
Martin Schmid <m.schmid@equitania.com>
2016-02-16 09:45:48 +0100

    [CHG] Optische Korrekturen

--- Scripts/containers.sh
+++ Scripts/containers.sh
@@ -8,9 +8,9 @@ echo "\$1 = $1"
 
 if [ "$1" == "start" ] || [ "$1" == "stop" ] || [ "$1" == "restart" ]; then
   # Datenbank Container
-  docker $1 db-containername
+  docker $1 containername-db
   ## MyOdoo Container
-  docker $1 myodoo-containername
+  docker $1 containername-myodoo
 fi
 
 docker ps
