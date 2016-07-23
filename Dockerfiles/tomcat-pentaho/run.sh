89b3653c66615e6b9567bfee108a3b81a2869ba0
Martin Schmid <m.schmid@equitania.com>
2016-07-23 22:09:14 +0200

    [ADD] Docker Build für Tomcat 8 und Pentado odoo

---
+++ Dockerfiles/tomcat-pentaho/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -f /.tomcat_admin_created ]; then
+    /create_tomcat_admin_user.sh
+fi
+
+export CATALINA_OPTS="$CATALINA_OPTS ${JAVA_OPTS}"
+
+exec ${CATALINA_HOME}/bin/catalina.sh run
