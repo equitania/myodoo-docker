da75594b5b5f5e55e71a4bf2bf776c391c19de7e
Martin Schmid <m.schmid@equitania.com>
2017-10-25 17:32:40 +0200

    [CHG] Anpassungen für http2

--- Scripts/nginx-conf/install_pagespeed_nginx.sh
+++ Scripts/nginx-conf/install_pagespeed_nginx.sh
@@ -6,8 +6,9 @@
 ## https://developers.google.com/speed/pagespeed/module/
 ## Debian ISO: https://cdimage.debian.org/cdimage/archive/8.9.0/amd64/iso-cd/
 ## https://github.com/jniltinho/ispconfig/tree/master/packages/debian/jessie
+## https://github.com/pagespeed/ngx_pagespeed/releases
 ## Run as root (sudo su)
-
+## Stand 25.10.2017
 
 echo "deb http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list.d/nginx.list
 echo "deb-src http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list.d/nginx.list
@@ -15,47 +16,38 @@ echo "deb-src http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/source
 curl http://nginx.org/keys/nginx_signing.key | apt-key add -
 
 
-NPS_VERSION=1.12.34.3-stable
-NPS_RELEASE_NUMBER=${NPS_VERSION/stable/}
-
-sed -i "s|# deb-src|deb-src|" /etc/apt/sources.list
 apt-get update
 apt-get install -y lsb-release dpkg-dev build-essential zlib1g-dev libpcre3 libpcre3-dev unzip curl
 
 cd ~
-mkdir -p ~/nginx_source/
-cd ~/nginx_source/
+mkdir -p ~/compile/nginx_source/
+cd ~/compile/nginx_source/
 apt-get source nginx
+apt-get build-dep nginx
 
-rm -rf /var/lib/apt/lists/
-apt-get update
-apt-get build-dep -y nginx
+mkdir -p ~/compile/ngx_pagespeed/
+cd ~/compile/ngx_pagespeed/
+wget https://github.com/pagespeed/ngx_pagespeed/archive/v1.12.34.3-stable.zip
+unzip v1.12.34.3-stable.zip
 
-sed -i "s|deb-src|# deb-src|" /etc/apt/sources.list
 
-cd ~
-wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}.tar.gz
-tar xvfz v${NPS_VERSION}.tar.gz
+cd ngx_pagespeed-1.12.34.3-stable/
+wget https://dl.google.com/dl/page-speed/psol/1.12.34.2-x64.tar.gz
+tar -xzvf 1.12.34.2-x64.tar.gz
 
-cd ngx_pagespeed-${NPS_VERSION}/
-psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_RELEASE_NUMBER}.tar.gz
-psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
-wget ${psol_url}
-tar -xzvf $(basename ${psol_url})
-
-sed -i "s|--with-http_auth_request_module|--with-http_auth_request_module --add-module=$HOME/ngx_pagespeed-${NPS_VERSION}|" $HOME/nginx_source/nginx-1.*.*/debian/rules
-cd ~/nginx_source/nginx-1.*.*/
+cd ~/compile/nginx_source/nginx-1.12.2/
+sed -i "s|--with-http_auth_request_module|--with-http_auth_request_module --add-module=/root/compile/ngx_pagespeed/ngx_pagespeed-1.12.34.3-stable|" /root/compile/nginx_source/nginx-1.12.2/debian/rules
 dpkg-buildpackage -b
 
 cd ../
 ## Von der nginx.conf wird ein Backup gemacht
 if [ -f /etc/nginx/nginx.conf ]; then service nginx stop; cp -aR /etc/nginx /etc/nginx_$$; fi
-dpkg --force-all -i nginx_1.*_all.deb nginx-common_1.*_all.deb nginx-full_1.*_amd64.deb
+dpkg --force-all -i nginx_1.12.2-1~jessie_amd64.deb
 ## Von der nginx.conf wird wiederhergestellt
 if [ -f "/etc/nginx_$$/nginx.conf" ]; then rm -rf /etc/nginx/*; cp -aR /etc/nginx_$$/* /etc/nginx/; fi
 
 
-## Para Finalizar
+## Finalizierung
 mkdir -p /var/ngx_pagespeed_cache && chmod 777 /var/ngx_pagespeed_cache
 
 
