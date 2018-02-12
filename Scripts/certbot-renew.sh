df1afc4bd2c82f527d7656275fc45ebceae0b31e
Martin Schmid <m.schmid@equitania.com>
2018-02-12 20:56:19 +0100

    [FIX] certbot-renew.sh um Logfile cleanup nginx erweitert.

--- Scripts/certbot-renew.sh
+++ Scripts/certbot-renew.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# Erneuert die von Let's encrypt erstellten Zertifikate
+# Erneuert die von Let's encrypt erstellten Zertifikate und nginx logs älter als 14 Tage löschen
 # Einstellung für crontab -e
-# Jeden Samstag Nacht um 3 Uhr sollen die Zertifikate geprüft werden
-# 00 03 * * 6  /root/certbot-renew.sh | tee /var/log/letsencrypt-renew.log
+# Jeden 15. des Monats um 3 Uhr sollen die Zertifikate geprüft werden
+#00 03 15 * *  /root/certbot-renew.sh | tee /var/log/letsencrypt-renew.log
+
 
 echo "nginx stop"
 /etc/init.d/nginx stop
 echo "certbot renew"
 /root/certbot/letsencrypt-auto renew
+echo "delete nginx log files older than 14 days"
+find /var/log/nginx/ -type f -mtime +14 | xargs rm
 echo "nginx start"
 /etc/init.d/nginx start
 /etc/init.d/nginx status
