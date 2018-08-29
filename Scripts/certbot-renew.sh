58d11101de217ed287881cb86bafc647edcbe8b4
Martin Schmid <m.schmid@equitania.com>
2018-08-29 20:10:07 +0200

    [FIX] OS relative paths

--- Scripts/certbot-renew.sh
+++ Scripts/certbot-renew.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Erneuert die von Let's encrypt erstellten Zertifikate und nginx logs älter als 14 Tage löschen
 # Einstellung für crontab -e
-# Jeden 15. des Monats um 3 Uhr sollen die Zertifikate geprüft werden
-#00 03 15 * *  /root/certbot-renew.sh | tee /var/log/letsencrypt-renew.log
+# Jeden Samstag um 3 Uhr sollen die Zertifikate geprüft werden
+#0 3 * * 6  /root/certbot-renew.sh | tee /var/log/letsencrypt-renew.log>/dev/null 2>&1
+
 
 
 echo "nginx stop"
 service nginx stop
 echo "certbot renew"
-/root/certbot/letsencrypt-auto renew
+$HOME/certbot/letsencrypt-auto renew
 echo "delete nginx log files older than 7 days = DSGVO konform"
 find /var/log/nginx/ -type f -mtime +7 | xargs rm
 echo "nginx start"
