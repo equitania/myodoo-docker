bc08c15fee0d69d0f78f4127e71cd07692387635
Release Manager <release@ownerp.io>
2018-09-06 18:35:51 +0000

    Update Mechanismus angepasst

--- Dockerfiles/v10-muster/build_myodoo.py
+++ Dockerfiles/v10-muster/build_myodoo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Mit diesem Skript wird mittels dem Release Manager ein neuer Server gebaut
-# Version 1.0.3
+# Version 1.0.4
 # Date 06.09.2018
 ##############################################################################
 #
@@ -25,59 +25,49 @@
 
 import os, csv, time
 
-_access_file = 'access_myodoo.txt'
 _build_path = '/opt/odoo'
 _release_file = 'release.file'
 
-if os.path.isfile(_access_file):
-    _accesscode = open(_access_file).readline().rstrip()
-    # Wenn accesscode gefüllt, wird versucht das Releasefile zu holen
-    if _accesscode != "":
-        print('Starting with build at ' + _build_path + ' with accesscode ' + _accesscode)
-        os.chdir(_build_path)
-        if not os.path.isfile(_access_file):
-            os.system('wget -q -O release.file https://v10.myodoo.de/get_csv_file/' + _accesscode)
-        while not os.path.isfile(_release_file):
-            time.sleep(0.1)
-        # Wenn Releasefile gefüllt ist, beginnt der Buildprozess
-        if os.stat(_release_file).st_size != 0:
-            _reader = csv.reader(open(_release_file, 'rb'))
-            _count = 1
-            for _row in _reader:
-                _column = _row[0]
-                _column = _column.replace(' ', '')
-                if _count == 1:
-                    print('url: ' + _column)
-                    _url = _column
-                    if _url == 'False':
-                        print('url is missing .. stop!')
-                        exit()
-                elif _count == 2:
-                    print('dockerimage: ' + _column)
-                elif _count == 3:
-                    if _column == 'False':
-                        print('kernel is missing .. stop!')
-                        exit()
-                    else:
-                        os.system('wget -qq ' + _url + '/' + _column)
-                        os.system('mkdir -p odoo-server/addons')
-                        while not os.path.isfile(_column):
-                            time.sleep(0.1)
-                        os.system('unzip -q ' + _column + ' -d odoo-server')
-                        print('kernel: ' + _column + ' loaded and installed..')
+if os.path.isfile(_release_file):
+    print('Starting with build at ' + _build_path)
+    os.chdir(_build_path)
+    # Wenn Releasefile gefüllt ist, beginnt der Buildprozess
+    if os.stat(_release_file).st_size != 0:
+        _reader = csv.reader(open(_release_file, 'rb'))
+        _count = 1
+        for _row in _reader:
+            _column = _row[0]
+            _column = _column.replace(' ', '')
+            if _count == 1:
+                print('url: ' + _column)
+                _url = _column
+                if _url == 'False':
+                    print('url is missing .. stop!')
+                    exit()
+            elif _count == 2:
+                print('dockerimage: ' + _column)
+            elif _count == 3:
+                if _column == 'False':
+                    print('kernel is missing .. stop!')
+                    exit()
                 else:
                     os.system('wget -qq ' + _url + '/' + _column)
+                    os.system('mkdir -p odoo-server/addons')
                     while not os.path.isfile(_column):
                         time.sleep(0.1)
-                    os.system('unzip -q ' + _column + ' -d odoo-server/addons')
-                    print('file: ' + _column + ' loaded and installed..')
-                _count += 1
-
-            print('Build finished!')
+                    os.system('unzip -q ' + _column + ' -d odoo-server')
+                    print('kernel: ' + _column + ' loaded and installed..')
+            else:
+                os.system('wget -qq ' + _url + '/' + _column)
+                while not os.path.isfile(_column):
+                    time.sleep(0.1)
+                os.system('unzip -q ' + _column + ' -d odoo-server/addons')
+                print('file: ' + _column + ' loaded and installed..')
+            _count += 1
         else:
             print('No valid release file :(')
-    else:
-        print('Not valid accesscode :(')
+            exit()
+    print('Build finished!')
     os.system('rm -f *.zip')
-    os.system('rm -f release.file')
+    #os.system('rm -f release.file')
     print('Cleanup and finished!')
