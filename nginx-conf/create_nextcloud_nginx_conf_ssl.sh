ee2fca54cf410732a9e23c92702f9ef9660fddad
Martin Schmid <m.schmid@equitania.de>
2019-01-18 10:45:37 +0100

    Update nginx-conf/create_nextcloud_nginx_conf_ssl.sh

--- nginx-conf/create_nextcloud_nginx_conf_ssl.sh
+++ nginx-conf/create_nextcloud_nginx_conf_ssl.sh
@@ -45,7 +45,7 @@ if [ "$mydomain" = "" ]; then
 fi
 
 if [ "$myport" = "" ]; then
-  echo "Insert the odoo port | Geben Sie den Port fuer Odoo ein:"
+  echo "Insert the odoo port | Geben Sie den Port fuer NextCloud ein:"
   read myport
 fi
 
