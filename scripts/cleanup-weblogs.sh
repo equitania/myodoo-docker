dd4428ea54594306bf9bea3bb0129556099e2e92
Martin Schmid <m.schmid@equitania.de>
2020-10-29 18:39:05 +0100

    Update cleanup-weblogs.sh

--- scripts/cleanup-weblogs.sh
+++ scripts/cleanup-weblogs.sh
@@ -3,6 +3,11 @@
 # Jeden Tag um 3 Uhr sollen die Logs gelöscht werden, die älter sind als 7 Tage
 # * 3 * * * /root/cleanup-weblogs.sh >> /var/log/cleanup-weblog.log >/dev/null 2>&1
 
+dt=$(date '+%d.%m.%Y %H:%M:%S');
+echo "#######################################"
+echo "Start at $dt"
+echo "#######################################"
+
 echo "delete nginx log files older than 7 days = DSGVO konform"
 find /var/log/nginx/ -type f -mtime +7 | xargs rm
 
