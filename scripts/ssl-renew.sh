c996f0001b9b75e074ab6cc5226062654a48f657
Martin Schmid <m.schmid@equitania.de>
2020-11-02 17:34:43 +0100

    Update ssl-renew.sh

--- scripts/ssl-renew.sh
+++ scripts/ssl-renew.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Erneuert die von Let's encrypt erstellten Zertifikate und nginx logs älter als 14 Tage löschen
+# Erneuert die von Let's encrypt erstellten Zertifikate
 # Einstellung für crontab -e
 # Renew certificates every wednesday at 0:00 h
-# 0 0 * * 3 /root/ssl-renew.sh >> /var/log/ssl-renew.log 2>&1
+# 0 0 * * 3 /root/ssl-renew.sh | tee /var/log/ssl-renew.log >/dev/null 2>&1
 
-echo -n "" > /var/log/ssl-renew.log
 
 dt=$(date '+%d.%m.%Y %H:%M:%S');
 echo "#######################################"
