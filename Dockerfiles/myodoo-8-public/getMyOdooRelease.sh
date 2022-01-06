057de568ad4dab08bc8396f41b09c9cbf71bbdbd
Martin Schmid <m.schmid@equitania.de>
2022-01-06 10:44:48 +0100

    [FIX] First cleanup 2022 - remove python 2.7 staff

--- Dockerfiles/myodoo-8-public/getMyOdooRelease.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Hole Ihre aktuelle Release vom Server
-myrelease=$(curl -k https://v8.myodoo.de/get_release_info/myodoopublic)
-echo $myrelease
-curl -k -o $myrelease https://release.myodoo.de/conf/$myrelease
-now=$(date +"%Y-%m-%d_%H-%M-%S")
-filename="myodoo.$now.release"
-mv myodoo.release $filename
-mv $myrelease myodoo.release
--- Dockerfiles/prepare-myodoo-v8/ReadMe.md
+++
@@ -1,8 +0,0 @@
-# For build of the docker image
-# For Odoo 8 powered by MyOdoo.de
-# Version 1.0.21
-# Date 21.09.2020
-docker build -t myodoo/prepare-v8:1.0.21 .
-docker push myodoo/prepare-v8:1.0.21
-
-#This is only a prepare script for the release images of myodoo.
